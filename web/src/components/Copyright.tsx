be672177847044a3e9735a16fb2bff82ebe6704c
guipaluri <guilhermepaluri@hotmail.com>
2023-05-17 23:52:56 -0300

    Adding user authentication on BE

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no NLW da{' '}
+      <a
+        className="underline transition-colors hover:text-gray-100"
+        href="http://rocketseat.com.br"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
