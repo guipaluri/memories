367badcb3904d951b321586087b7af2ac5a53e56
guipaluri <guilhermepaluri@hotmail.com>
2023-05-23 16:52:12 -0300

    Adding page layout + middleware on Web

---
+++ web/src/app/api/auth/logout/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(req: NextRequest) {
+  const redirectURL = new URL('/', req.url)
+
+  return NextResponse.redirect(redirectURL, {
+    headers: {
+      'Set-Cookie': `token=; Path=/; max-age=0`,
+    },
+  })
+}
